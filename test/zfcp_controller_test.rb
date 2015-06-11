bd5c65d65fd52053323f53b7413f488984801d2e
Stefan Schubert <schubi@suse.de>
2015-06-11 17:31:27 +0200

    additional test

--- test/zfcp_controller_test.rb
+++ test/zfcp_controller_test.rb
@@ -69,6 +69,9 @@ describe "ZFCPController" do
       {"controller_id" => "0.0.f800"},
       {"controller_id" => "0.0.f900"}]}
 
+    expect(Yast::ZFCPController.Import(import_data)).to eq(true)
+    expect(Yast::ZFCPController.GetDeviceIndex("0.0.f800")).to eq(2)
+
   end
 
 end
