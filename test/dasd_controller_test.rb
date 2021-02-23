7acbc45ba3636e7b2d0eec110bc18ab0ed0607b1
Josef Reidinger <jreidinger@suse.cz>
2021-02-23 22:05:17 +0100

    more mocking for disk formatting

--- test/dasd_controller_test.rb
+++ test/dasd_controller_test.rb
@@ -234,7 +234,10 @@ describe Yast::DASDController do
 
   describe "#FormatDisks" do
     it "formats the given disks using dasdfmt" do
-      allow(Yast::SCR).to receive(:Read).with(path(".process.running"), 100).and_return(true)
+      allow(Yast::SCR).to receive(:Read).with(path(".process.running"), 100).and_return(true, false)
+      allow(Yast::SCR).to receive(:Read).with(path(".process.status"), 100).and_return(0)
+      allow(Yast::SCR).to receive(:Read).with(path(".process.read_line"), 100).and_return("0")
+      allow(Yast::SCR).to receive(:Read).with(path(".process.read_line_stderr")).and_return(nil)
       expect(Yast::SCR).to receive(:Execute).with(
         path(".process.start_shell"), "/sbin/dasdfmt -Y -P 1 -b 4096 -y -r 10 -m 10 -f '/dev/dasda'"
       ).and_return(100)
